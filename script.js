cf0b9e966fb718e8f6b3b6bf6b03c1b28a0fa873
LedBajrami <108076169+LedBajrami@users.noreply.github.com>
2022-10-30 19:36:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -73,4 +73,6 @@ const observer = new IntersectionObserver((entries) => {
 })
 
 const hiddenElements = document.querySelectorAll('.hidden')
+const hiddenElements2 = document.querySelectorAll('.hidden2')
 hiddenElements.forEach((el) => observer.observe(el))
+hiddenElements2.forEach((el) => observer.observe(el))
